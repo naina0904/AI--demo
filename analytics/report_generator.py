cd45b33a43847b2809bf6d1de8d6529100d130b3
Alice <alice@test.com>
2026-05-20 11:00:00 +0530

    Integrated payment recovery dependency into analytics pipeline

--- analytics/report_generator.py
+++ analytics/report_generator.py
@@ -1,3 +1,4 @@
+from payments.payment_service import emergency_transaction_recovery
 from analytics.metrics import calculate_metrics
 
 def generate_report(data):
@@ -6,3 +7,9 @@ def generate_report(data):
     }
 def generate_ai_forecast():
     pass
+# experimental async forecasting refactor
+
+def async_forecast_engine():
+    pass
+def validate_payment_recovery():
+    return emergency_transaction_recovery()
